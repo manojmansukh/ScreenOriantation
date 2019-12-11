4fa02a819420828490ab1beb2f4feac61023810c
manojmansukh <manojmansukh7@gmail.com>
2019-12-11 11:28:46 +0530

    screen oriantation done

--- App.js
+++ App.js
@@ -1,114 +1,140 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import { View, Text, Dimensions, Image,TouchableOpacity, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, TextInput } from 'react-native';
+import React, { Component } from 'react'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import screen from './src/screen'
+export default class App extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "FunDoo",
+      headerStyle: { backgroundColor: "white" },
+      headerTitleStyle: { textAlign: "left", flex: 1 }
+    };
+  };
+  constructor() {
+    super();
+    this.state = { orientation: '' }
+  }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  getOrientation = () => {
+    if (this.refs.rootView) {
+      if (Dimensions.get('window').width < Dimensions.get('window').height) {
+        this.setState({ orientation: 'portrait' });
+      }
+      else {
+        this.setState({ orientation: 'landscape' });
+      }
+    }
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+  componentDidMount() {
+    this.getOrientation();
+
+    Dimensions.addEventListener('change', () => {
+      this.getOrientation();
+    });
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.innerView}>
+        <ScrollView style={{ width: '100%', height: '100%' }} >
+
+          <View ref="rootView" style={[styles.container, { backgroundColor: (this.state.orientation == 'portrait') ? '#1B5E20' : '#006064' }]}>
+            <Text style={styles.loginText}>{this.state.orientation.toUpperCase()} VIEW</Text>
+          </View>
+
+          <View style={{ flexDirection: 'row',flexWrap:"wrap",justifyContent:'center',alignItems:'center' }}>
+
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <Image source={require('/Users/admin/Documents/ScreenOrientation/login.png')}
+                style={{ width: 200, height: 200,margin:40 }} />
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+
+
+            <View style={{ justifyContent: 'center' }}>
+              <TextInput style={styles.input}
+                underlineColorAndroid="transparent"
+                placeholder="Email"
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="email-address"
+                onChangeText={(text) => this.validateEmail(text)} />
+              <Text style={styles.error}>{this.state.emailError}</Text>
+
+              <TextInput style={styles.input}
+                underlineColorAndroid="transparent"
+                placeholder="Password"
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry={true}
+                onChangeText={(text) => this.validatePass(text)} />
+              <Text style={styles.error}>{this.state.passError}</Text>
+
+
+            <TouchableOpacity
+              style={styles.submitButton}
+              onPress={this.handleLogin}>
+              <Text style={styles.submitButtonText}> Login </Text>
+            </TouchableOpacity>
+
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+      </KeyboardAvoidingView>
+    );
+  }
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create(
+  {
+    container:
+    {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: (Platform.OS === 'ios') ? 20 : 0
+    },
+    innerView: {
+      flex: 1,
+      width: '100%',
+      backgroundColor: "white",
+      // justifyContent: "flex-start",
+      // alignItems: "center",
+    },
+    text:
+    {
+      fontSize: 22,
+      color: 'white',
+      fontWeight: 'bold'
+    },
+    input: {
+      marginTop: 15,
+      maxWidth:'40%',
+      minWidth:300,
+      borderColor: '#7a42f4',
+      alignSelf: "center",
+      borderWidth: 1
+    },
+    loginText: {
+      display: 'flex',
+      alignSelf: "center",
+      paddingRight: 40,
+      marginHorizontal: 20,
+      marginVertical: 20,
+      fontSize: 30,
+      fontWeight: "bold",
+    },
+    submitButton: {
+      alignSelf: "center",
+      backgroundColor: '#7a42f4',
+      margin: 15,
+      height: 40,
+      width: 200,
+      fontSize: 30,
+    },
+    submitButtonText: {
+      color: 'white',
+      padding: 10,
+      alignSelf: "center"
+    },
+  });
--- src/screen.js
+++ src/screen.js
@@ -1,63 +1,190 @@
-import { View, Text, StyleSheet, Dimensions, Platform } from 'react-native';
- 
-export default class App extends Component<{}>
-{
-  constructor()
-  {
-    super();
- 
-    this.state = { orientation: '' }
+import React, { Component } from "react";
+import { View, TouchableOpacity, Text, SafeAreaView, KeyboardAvoidingView, ScrollView, StyleSheet, TextInput } from "react-native";
+export default class screen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "FunDoo",
+      headerStyle: { backgroundColor: "white" },
+      headerTitleStyle: { textAlign: "left", flex: 1 }
+    };
+  };
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+      emailError: null,
+      passError:null,
+
+    }
+    // this.handleLogin = this.handleLogin.bind(this);
   }
- 
-  getOrientation = () =>
-  {
-    if( this.refs.rootView )
-    {
-        if( Dimensions.get('window').width < Dimensions.get('window').height )
-        {
-          this.setState({ orientation: 'portrait' });
-        }
-        else
-        {
-          this.setState({ orientation: 'landscape' });
-        }
+
+  validateEmail = (text) => {
+    console.log(text);
+    let email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   
+    if (email.test(text) === false) {
+      this.setState({ emailError: 'Email is Not Correct' });
+      return false;
+    }
+    else {
+      //email correct
+      this.setState({ email: text })
+      this.setState({ emailError: null });
+    }
+  } 
+  validatePass = (text) => {
+    console.log(text);
+    let pass = /^[#\w@_-]{8,20}$/
+    if (pass.test(text) === false) {
+      this.setState({ passError: 'enter valid pass' });
+      this.setState({ password: text })
+      return false;
+    }
+    else {
+      //email correct
+      this.setState({ password: text })
+      this.setState({ passError: null });
+
     }
   }
- 
-  componentDidMount()
-  {
-    this.getOrientation();
-    
-    Dimensions.addEventListener( 'change', () =>
-    {
-      this.getOrientation();
-    });
-  }
- 
-  render()
-  {
-    return(
-      <View ref = "rootView" style = {[ styles.container, { backgroundColor: ( this.state.orientation == 'portrait' ) ? '#1B5E20' : '#006064' }]}>
-        <Text style = { styles.text }>{ this.state.orientation.toUpperCase() } VIEW</Text>
-      </View>
+
+
+  // handleLogin = () =>{
+  //    console.log("hii");
+  //    console.log(this.state.email);
+
+  //   firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+  //     .then(() => this.props.navigation.navigate('Welcome'))
+  //     .catch((error) => {
+  //       alert("Enter the valid Details...!", error)
+  //       var errorCode = error.code;
+  //       var errorMessage = error.errorMessage;
+  //       console.log('Error code :' + errorCode);
+  //       console.log('Error Msg :' + errorMessage);
+  //     });
+  // }
+  render() {
+    return (
+
+      <KeyboardAvoidingView style={styles.innerView}>
+        <ScrollView>
+          <Text style={styles.loginText}>
+            Login
+            </Text>
+
+          <View>
+            <TextInput style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Email"
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              onChangeText={(text) => this.validateEmail(text)} />
+            <Text style={styles.error}>{this.state.emailError}</Text>
+
+            <TextInput style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              secureTextEntry={true}
+              onChangeText={(text) => this.validatePass(text)} />
+              <Text style={styles.error}>{this.state.passError}</Text>
+
+
+
+            {/* 
+              <PasswordInputText style={{height: 90,width: 300,alignSelf: "center",height: 40,margin: 1,borderWidth: 1,}}
+              onChangeText={(text) => this.setState({ password: text })} >
+              </PasswordInputText> */}
+
+            <TouchableOpacity
+              style={styles.submitButton}
+              onPress={alert("Registration Complete Succesfully...!")}>
+              <Text style={styles.submitButtonText}> Login </Text>
+            </TouchableOpacity>
+
+            {/* <TouchableOpacity onPress={() => { this.props.navigation.navigate('ForgotPass') }}>
+              <Text style={styles.text}>
+                Forgot Password?
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => { this.props.navigation.navigate('Registration') }}>
+              <Text style={styles.text}>
+                Sign UP
+              </Text>
+            </TouchableOpacity>
+ */}
+
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
     );
   }
 }
- 
-const styles = StyleSheet.create(
-{
-  container:
-  {
+const styles = StyleSheet.create({
+  wrapper: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: ( Platform.OS === 'ios' ) ? 20 : 0
+    backgroundColor: "#daebf4",
+
   },
- 
-  text:
-  {
-    fontSize: 22,
+  innerView: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: "white",
+    // justifyContent: "flex-start",
+    // alignItems: "center",
+  },
+  input: {
+    marginTop: 15,
+    width: '90%',
+    borderColor: '#7a42f4',
+    alignSelf: "center",
+    borderWidth: 1
+  },
+  loginText: {
+    display: 'flex',
+    alignSelf: "center",
+    paddingRight: 40,
+    marginHorizontal: 20,
+    marginVertical: 30,
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  submitButton: {
+    alignSelf: "center",
+    backgroundColor: '#7a42f4',
+    margin: 15,
+    height: 40,
+    width: 300,
+    fontSize: 30,
+  },
+  submitButtonText: {
     color: 'white',
-    fontWeight: 'bold'
+    padding: 10,
+    alignSelf: "center"
+  },
+  text: {
+    flexDirection: "row",
+    fontSize: 15,
+    alignSelf: "center",
+    //alignItems:"flex-end"
+  },
+  error: {
+    color: 'red',
+    width:'90%',
+    marginTop:1,
+    marginHorizontal:30,
+    flexDirection: "row",
+    fontSize: 11,
+    
   }
+
+
+
 });
